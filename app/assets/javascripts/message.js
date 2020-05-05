ea61889f4acc64e39d430666ccf00dd74a79d199
hanaji-naji <hanaji.halloween@gmail.com>
2020-05-05 12:37:35 +0900

    add_auto_appdate_function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="message">
+       `<div class="message", data-message-id="${message.id}">
           <div class="message__info">
             <div class="message__info__user-name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       var html =
-       `<div class="message">
+       `<div class="message", data-message-id="${message.id}">
           <div class="message__info">
             <div class="message__info__user-name">
               ${message.user_name}
@@ -39,6 +39,29 @@ $(function(){
       return html;
     };
   }
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.message-list').append(insertHTML);
+        $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -63,4 +86,8 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+  
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
